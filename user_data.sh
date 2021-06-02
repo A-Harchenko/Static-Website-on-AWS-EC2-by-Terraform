507fa2e3a801029f797cf8ebb5bb7312f9452d85
Anastasia Harchenko <84336984+HarAnasVlad@users.noreply.github.com>
2021-06-03 00:53:02 +0400

    Update 1.1
    - Replaced the commands in user_data for ubuntu instead of linux

--- user_data.sh
+++ user_data.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
-yum -y update
-yum -y install httpd
-echo "<h1>It was an interesting task</h1>"> /var/www/html/index.html
-sudo service httpd start
-chkconfig httpd on
+sudo apt install apache2 -y
+cd /var/www/html
+sudo rm index.html
+sudo touch index.html
+cat << EOF | sudo tee -a index.html
+<html>
+<head>
+<title>Finish</title>
+<style>
+body {
+background: #F2F2F2;
+max-width: 900px;
+margin: 10px auto;
+padding: 30px;
+}
+h1{
+color: #4C4C4C;
+padding-bottom: 20px;
+margin-bottom: 20px;
+border-bottom: 2px solid #BEBEBE;
+}
+p{
+font:italic;
+}
+</style>
+</head>
+<body>
+<h1>It was an interesting task</h1>
+</body>
+</html>
+EOF
+sudo systemctl start apache2 
+sudo systemctl enable apache2 
